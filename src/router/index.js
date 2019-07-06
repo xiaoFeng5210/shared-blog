e6e673c90ac30443e8d763bc0f302699416b2e6a
xiaofeng <804875728@qq.com>
2019-07-06 19:38:58 +0800

    页面部署

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/pages/login/login.vue'
+
 
 Vue.use(Router)
 
@@ -10,6 +12,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path:'/login',
+      component:Login
     }
   ]
 })
