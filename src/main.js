02a879c13479c98b80a18ac1c15afea8f86b6cd8
xiaofeng <804875728@qq.com>
2019-07-16 16:26:03 +0800

    页面部分完成

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
+import Util from '@/helpers/util'
+
+
+Vue.use(ElementUI)
+Vue.use(Util)
 
 Vue.config.productionTip = false
 
@@ -10,6 +18,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
