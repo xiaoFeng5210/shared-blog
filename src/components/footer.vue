e32cd9b2f66499ee4e7482b0d398f306e04f277c
xiaofeng <804875728@qq.com>
2019-07-16 16:52:26 +0800

    基础页面完善

---
+++ src/components/footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <p>© 欢迎来到张庆风的共享博客</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
+
+</style>
